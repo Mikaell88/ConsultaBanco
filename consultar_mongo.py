a884287fa04d0f6eea43f405d90177af6df4ea25
Mikaell88 <Mikaelsby@outlook.com.br>
2025-06-29 15:52:02 -0300

    Atualização

--- consultar_mongo.py
+++ consultar_mongo.py
@@ -1,32 +1,35 @@
 from pymongo import MongoClient
 
-# Conexão (com sua URI atualizada)
+# Conexão Com o MongoDB
 client = MongoClient("mongodb+srv://SEU_USUARIO:SUA_SENHA@cluster0.4zapnmf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Seleciona o banco e a coleção
 db = client['bigdata_games']
 colecao = db['vendas_jogos']
 
-# Consulta todos os documentos (retorna um cursor)
+#Consulta todos os documentos
 #resultados = colecao.find()
-
 #for documento in resultados:
 #    print(documento)
+#-------------------------------------
+
 
 #filtro = {"year": 2015}
 #resultados = colecao.find(filtro)
-
 #for doc in resultados:
 #    print(doc)
+#-------------------------------------
 
 
 #resultados = colecao.find().limit(5)
-
 #for doc in resultados:
 #    print(doc)
+#-------------------------------------
 
-#jogo = colecao.find_one({"name": "New Super Mario Bros. Wii"})
-#print(jogo)
+
+jogo = colecao.find_one({"name": "New Super Mario Bros. Wii"})
+print(jogo)
+#-------------------------------------
 
 
 #filtro = {"year": 2015, "platform": "PS4"}
